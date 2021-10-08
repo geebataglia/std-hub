3c4cfe0a56bbd71ed27d8a58fcc45a70e2f5430c
Guilherme Bataglia <guilhermebataglia@outlook.ie>
2021-10-08 12:05:30 -0300

    20211008 Form validation & prevent mistakes

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -19,15 +19,16 @@ for(var i = 0; i < tabPaciente.length; i++){
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if(altura <= 0 || altura >= 2.50){
+    // Os if's abaixo somente vão existir para dados importados ou ja existentes. 
+    if(!alturaEhValida){
         alturaEhValida = false;
         tdImc.textContent = "Altura Inválida!";
         paciente.classList.add("paciente-invalido");
     }
-    if(peso <= 0 || peso >= 400){
+    if(!pesoEhValido){
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido!";
         paciente.classList.add("paciente-invalido");
@@ -35,6 +36,22 @@ for(var i = 0; i < tabPaciente.length; i++){
     if(alturaEhValida && pesoEhValido){
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso <= 400){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 2.50){
+        return true;
+    }else{
+        return false;
     }
 }
 
--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -7,11 +7,31 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
         var paciente = obtemPacienteDoFormulario(form);
         //Cria uma linha separada por colunas das informações digitadas
         var novoPaciente = montarTabela(paciente);
+
+        var erros = validaPaciente(paciente);
+        
+        if(erros.length > 0){
+            showMsgErro(erros);
+            return;
+        }
         //Adiciona na tabela as informações coletadas
         var tabela = document.querySelector("#tabela-pacientes");
             tabela.appendChild(novoPaciente);
             form.reset();
+            var msgErro = document.querySelector("#msg-erros");
+            msgErro.innerHTML = "";
     });
+
+function showMsgErro(erros){
+    var ul = document.querySelector("#msg-erros");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form){
 
@@ -43,4 +63,34 @@ function fillTabela(dado, classe){
     td.textContent = dado;
     td.classList.add(classe);
     return td
+}
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if(paciente.nome.length == 0){
+        erros.push("O nome precisa ser preenchido!");
+    }
+
+    if(!validaPeso(paciente.peso)){
+        erros.push("Peso inválido!");
+    } 
+    
+    if(!validaAltura(paciente.altura)){
+        erros.push("Altura inválida!");
+    } 
+
+    if(paciente.gordura.length == 0){
+        erros.push("Campo de %gordura é obrigatório!");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("Campo de peso é obrigatório!")
+    }
+    if(paciente.altura.length == 0){
+        erros.push("Campo de altura é obrigatório!")
+    }
+
+    return erros;
 }
